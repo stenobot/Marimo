0eaac52d1c584dc5e80aa017850319e04f8a48fe
Matt <bionicleper@hotmail.com>
2017-05-20 12:27:01 -0700

    Updated map to move elevator off floor layer, reimported, created elevator sprite and prefab

---
+++ src/Marimo/Marimo/Assets/Scripts/Elevator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Controls the elevators in the game
+/// </summary>
+public class Elevator : MonoBehaviour {
+    public Vector2 StartLocation;
+    public Vector2 StopLocation;
+
+	// Use this for initialization
+	void Start () {
+        // Ensure the elevator is at the start position
+        transform.localPosition = StartLocation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
