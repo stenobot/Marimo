d2b710ecda68713f7638a5356fc68418912f909e
Matt <bionicleper@hotmail.com>
2017-06-03 15:24:29 -0700

    Updated raise body animation to account for thought bubble object. Fixed thought bubble scale in RobotController

--- src/Marimo/Marimo/Assets/Scripts/RobotController.cs
+++ src/Marimo/Marimo/Assets/Scripts/RobotController.cs
@@ -292,7 +292,7 @@ public class RobotController : MonoBehaviour
             int scale = xAxisInput < 0 ? -1 : 1;
             Animator_Treads.transform.localScale = new Vector2(scale, 1);
             Animator_Body.transform.localScale = new Vector2(scale, 1);
-            //Animator_ThoughtBubble.transform.localScale = new Vector3(-scale, 1);
+            Animator_ThoughtBubble.transform.localScale = new Vector3(scale, 1);
 
             // Add force to the player
             m_rigidBody.AddForce(Vector2.right * MoveForce * xAxisInput);
