95881691ead922c7eeb78b2a48c398a9c9bfc858
Matt <bionicleper@hotmail.com>
2017-12-17 13:18:27 -0800

    Added animations to PipeTile. Can be started by setting animation speed (it'll start on all pipes). Just gotta work out a way to trigger it.

--- src/Marimo/Marimo/Assets/Scripts/Tiles/PipeTile.cs
+++ src/Marimo/Marimo/Assets/Scripts/Tiles/PipeTile.cs
@@ -5,15 +5,15 @@ using UnityEngine.Tilemaps;
 using UnityEditor;
 using System;
 
-public class PipeTile : Tile 
+public class PipeTile : Tile
 {
-	private const int SIDE_LEFT = 0;
-	private const int SIDE_BOTTOM = 1;
-	private const int SIDE_TOP = 2;
-	private const int SIDE_RIGHT = 3;
+    private const int SIDE_LEFT = 0;
+    private const int SIDE_BOTTOM = 1;
+    private const int SIDE_TOP = 2;
+    private const int SIDE_RIGHT = 3;
 
-	// array of all pipe sprites
-	public Sprite[] PipeSprites;
+    // array of all pipe sprites
+    public Sprite[] PipeSprites;
 
     [Serializable]
     public struct DrainAnim
@@ -23,193 +23,272 @@ public class PipeTile : Tile
     }
 
     public DrainAnim[] DrainAnimations;
+    private Dictionary<Enums.PipeTileSprite, Sprite[]> m_drainSprites = new Dictionary<Enums.PipeTileSprite, Sprite[]>();
 
-	public override void RefreshTile(Vector3Int position, ITilemap tilemap)
-	{
-		// loop through grid surrounding a tile
-		for (int x = -1; x <= 1; x++)
-		{
-			for (int y = -1; y <= 1; y++) 
-			{
-				Vector3Int currPos = new Vector3Int(position.x + x, position.y + y, position.z);
+    public override bool StartUp(Vector3Int position, ITilemap tilemap, GameObject go)
+    {
+        if (m_drainSprites.Count == 0)
+            // Populate dictionary for efficient lookups
+            foreach (DrainAnim anim in DrainAnimations)
+                m_drainSprites.Add(anim.PipeSpriteType, anim.AnimSprites);
 
-				// if tile has an existing pipe tile, call RefreshTile 
-				if (HasPipeTile(tilemap, currPos))
-					tilemap.RefreshTile(currPos);
-			}
-		}
-	}
+        return base.StartUp(position, tilemap, go);
+    }
 
-	/// <summary>
-	/// Gets the tile data. This gets fired each time RefreshTile is called on a tilemap position.
-	/// </summary>
-	/// <param name="position">Position.</param>
-	/// <param name="tilemap">Tilemap.</param>
-	/// <param name="tileData">Tile data.</param>
-	public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
-	{
-		// four adjacent tiles, are they pipes?
-		bool[] AdjacentPipeTiles = new bool[4];
+    public override void RefreshTile(Vector3Int position, ITilemap tilemap)
+    {
+        // loop through grid surrounding a tile
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                Vector3Int currPos = new Vector3Int(position.x + x, position.y + y, position.z);
 
-		int index = 0;
+                // if tile has an existing pipe tile, call RefreshTile 
+                if (HasPipeTile(tilemap, currPos))
+                    tilemap.RefreshTile(currPos);
+            }
+        }
+    }
 
-		// loop through grid surrounding tile
-		for (int x = -1; x <= 1; x++)
-		{
-			for (int y = -1; y <= 1; y++) 
-			{
-				// if tile is not one of four adjacent tiles, do nothing
-				if (!IsAdjacentTile(x,y)) 
-					continue;
+    /// <summary>
+    /// Gets the tile data. This gets fired each time RefreshTile is called on a tilemap position.
+    /// </summary>
+    /// <param name="position">Position.</param>
+    /// <param name="tilemap">Tilemap.</param>
+    /// <param name="tileData">Tile data.</param>
+    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+    {
+        // four adjacent tiles, are they pipes?
+        bool[] AdjacentPipeTiles = new bool[4];
 
-				// if adjacent tile is a pipe, set array index to true
-				if (HasPipeTile(tilemap, new Vector3Int(position.x + x, position.y + y, position.z)))
-					AdjacentPipeTiles[index] = true;
-				else
-					AdjacentPipeTiles[index] = false;
+        int index = 0;
 
-				index++;
-			}
-		}
+        // loop through grid surrounding tile
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                // if tile is not one of four adjacent tiles, do nothing
+                if (!IsAdjacentTile(x, y))
+                    continue;
 
-		// change the current sprite
-		tileData.sprite = PipeSprites[(int)AdjustedTileSprite(AdjacentPipeTiles)];
-	}
+                // if adjacent tile is a pipe, set array index to true
+                if (HasPipeTile(tilemap, new Vector3Int(position.x + x, position.y + y, position.z)))
+                    AdjacentPipeTiles[index] = true;
+                else
+                    AdjacentPipeTiles[index] = false;
 
-	/// <summary>
-	/// Adjusts the tile sprite based on it's adjacent tile
-	/// </summary>
-	/// <returns>The correct tile sprite</returns>
-	/// <param name="AdjacentPipeTiles">Array of all four adjacent tiles "is pipe" status</param>
-	private Enums.PipeTileSprite AdjustedTileSprite(bool[] AdjacentPipeTiles)
-	{
-		if (AdjacentPipeTiles[SIDE_LEFT]) 
-		{ 	
-			if (AdjacentPipeTiles[SIDE_BOTTOM]) 
-			{ 
-				if (AdjacentPipeTiles[SIDE_TOP]) 
-				{ 
-					if (AdjacentPipeTiles[SIDE_RIGHT]) 
-					{ 
-						return Enums.PipeTileSprite.OpenAll; 
-					} else // right no
-					{
-						return Enums.PipeTileSprite.OpenRight; 
-					}
-				} else // top no
-				{
-					if (AdjacentPipeTiles[SIDE_RIGHT]) 
-					{
-						return Enums.PipeTileSprite.OpenTop; 
-					} else // right no
-					{
-						return Enums.PipeTileSprite.CornerTopRight; 
-					}
-				}
-			} else // bottom no
-			{
-				if (AdjacentPipeTiles[SIDE_TOP]) 
-				{
-					if (AdjacentPipeTiles[SIDE_RIGHT]) 
-					{
-						return Enums.PipeTileSprite.OpenBottom; 
-					} else // right no
-					{
-						return Enums.PipeTileSprite.CornerBottomRight; 
-					}
-				} else // top no
-				{
-					if (AdjacentPipeTiles [SIDE_RIGHT]) 
-					{ 
-						return Enums.PipeTileSprite.Horizontal; 
-					} 
-					else // right no
-					{
-						return Enums.PipeTileSprite.CapRight; 
-					}
-				}
-			}
-		} else // left no
-		{
-			if (AdjacentPipeTiles[SIDE_BOTTOM]) 
-			{ 
-				if (AdjacentPipeTiles[SIDE_TOP]) 
-				{ 
-					if (AdjacentPipeTiles[SIDE_RIGHT]) 
-					{ 
-						return Enums.PipeTileSprite.OpenLeft; 
-					} else // right no
-					{
-						return Enums.PipeTileSprite.Vertical; 
-					}
-				} else // top no
-				{
-					if (AdjacentPipeTiles[SIDE_RIGHT]) 
-					{
-						return Enums.PipeTileSprite.CornerTopLeft; 
-					} else // right no
-					{
-						return Enums.PipeTileSprite.CapTop; 
-					}
-				}
-			} else // bottom no
-			{
-				if (AdjacentPipeTiles[SIDE_TOP]) 
-				{
-					if (AdjacentPipeTiles[SIDE_RIGHT]) 
-					{
-						return Enums.PipeTileSprite.CornerBottomLeft; 
-					} else // right no
-					{
-						return Enums.PipeTileSprite.CapBottom; 
-					}
-				} else // top no
-				{
-					if (AdjacentPipeTiles [SIDE_RIGHT]) 
-					{ 
-						return Enums.PipeTileSprite.CapLeft; 
-					} 
-				}
-			}
-		}
+                index++;
+            }
+        }
 
-		return Enums.PipeTileSprite.Horizontal; 
-	}
+        // change the current sprite
+        tileData.sprite = PipeSprites[(int)AdjustedTileSprite(AdjacentPipeTiles)];
+    }
 
-	/// <summary>
-	/// Determines whether this instance is an adjacent tile (as opposed to a corner)
-	/// </summary>
-	/// <returns><c>true</c> if this instance is an adjacent tile; otherwise, <c>false</c>.</returns>
-	/// <param name="x">The x coordinate from center</param>
-	/// <param name="y">The y coordinate from center</param>
-	private bool IsAdjacentTile(int x, int y)
-	{
-		return ((x == -1 && y == 0) || (x == 0 && y == -1) || (x == 0 && y == 1) || (x == 1 && y == 0));
-	}
 
-	/// <summary>
-	/// Determines whether this instance has a pipe tile at the specified tilemap position.
-	/// </summary>
-	/// <returns><c>true</c> if this instance has a pipe tile the specified tilemap position; otherwise, <c>false</c>.</returns>
-	/// <param name="tilemap">The current tilemap</param>
-	/// <param name="position">the position to check</param>
-	private bool HasPipeTile(ITilemap tilemap, Vector3Int position)
-	{
-		return tilemap.GetTile(position) == this;
-	}
+    /// <summary>
+    /// Gets the animation data for the tile
+    /// </summary>
+    /// <param name="location">The tile location</param>
+    /// <param name="tileMap">The tilemap</param>
+    /// <param name="tileAnimationData">The <see cref="TileAnimationData"/> object to place the output</param>
+    /// <returns>Boolean indicating success</returns>
+    public override bool GetTileAnimationData(Vector3Int location, ITilemap tileMap, ref TileAnimationData tileAnimationData)
+    {
+        TileData data = new TileData();
+        GetTileData(location, tileMap, ref data);
+        Enums.PipeTileSprite tileType = GetTileType(data);
 
-	#if UNITY_EDITOR
+        if (m_drainSprites[tileType].Length > 0)
+        {
+            tileAnimationData.animatedSprites = m_drainSprites[tileType];
+            tileAnimationData.animationSpeed = 0f;
+            tileAnimationData.animationStartTime = 0;
+            return true;
+        }
+        return false;
+    }
 
-	[MenuItem("Assets/Create/Tiles/PipeTile")]
-	public static void CreatePipeTile()
-	{
-		string path = EditorUtility.SaveFilePanelInProject ("Save Pipe Tile", "New Pipe Tile", "asset", "Save Pipe Tile", "Assets");
+    private Enums.PipeTileSprite GetTileType(TileData data)
+    {
+        Enums.PipeTileSprite result = Enums.PipeTileSprite.Horizontal;
+        Debug.Log(data.sprite.name);
+        switch (data.sprite.name.ToLower())
+        {
+            case "pipe_corner_bottom_left":
+                return Enums.PipeTileSprite.CornerBottomLeft;
+            case "pipe_corner_bottom_right":
+                return Enums.PipeTileSprite.CornerBottomRight;
+            case "pipe_corner_top_left":
+                return Enums.PipeTileSprite.CornerTopLeft;
+            case "pipe_corner_top_right":
+                return Enums.PipeTileSprite.CornerTopRight;
+            case "pipe_horizontal":
+                return Enums.PipeTileSprite.Horizontal;
+            case "pipe_open_bottom":
+                return Enums.PipeTileSprite.OpenBottom;
+            case "pipe_open_left":
+                return Enums.PipeTileSprite.OpenLeft;
+            case "pipe_open_right":
+                return Enums.PipeTileSprite.OpenRight;
+            case "pipe_open_top":
+                return Enums.PipeTileSprite.OpenTop;
+            case "pipe_vertical":
+                return Enums.PipeTileSprite.Vertical;
+        }
 
-		if (path == "")
-			return;
+        return result;
+    }
 
-		AssetDatabase.CreateAsset (ScriptableObject.CreateInstance<PipeTile> (), path);
-	}
+    /// <summary>
+    /// Adjusts the tile sprite based on it's adjacent tile
+    /// </summary>
+    /// <returns>The correct tile sprite</returns>
+    /// <param name="AdjacentPipeTiles">Array of all four adjacent tiles "is pipe" status</param>
+    private Enums.PipeTileSprite AdjustedTileSprite(bool[] AdjacentPipeTiles)
+    {
+        if (AdjacentPipeTiles[SIDE_LEFT])
+        {
+            if (AdjacentPipeTiles[SIDE_BOTTOM])
+            {
+                if (AdjacentPipeTiles[SIDE_TOP])
+                {
+                    if (AdjacentPipeTiles[SIDE_RIGHT])
+                    {
+                        return Enums.PipeTileSprite.OpenAll;
+                    }
+                    else // right no
+                    {
+                        return Enums.PipeTileSprite.OpenRight;
+                    }
+                }
+                else // top no
+                {
+                    if (AdjacentPipeTiles[SIDE_RIGHT])
+                    {
+                        return Enums.PipeTileSprite.OpenTop;
+                    }
+                    else // right no
+                    {
+                        return Enums.PipeTileSprite.CornerTopRight;
+                    }
+                }
+            }
+            else // bottom no
+            {
+                if (AdjacentPipeTiles[SIDE_TOP])
+                {
+                    if (AdjacentPipeTiles[SIDE_RIGHT])
+                    {
+                        return Enums.PipeTileSprite.OpenBottom;
+                    }
+                    else // right no
+                    {
+                        return Enums.PipeTileSprite.CornerBottomRight;
+                    }
+                }
+                else // top no
+                {
+                    if (AdjacentPipeTiles[SIDE_RIGHT])
+                    {
+                        return Enums.PipeTileSprite.Horizontal;
+                    }
+                    else // right no
+                    {
+                        return Enums.PipeTileSprite.CapRight;
+                    }
+                }
+            }
+        }
+        else // left no
+        {
+            if (AdjacentPipeTiles[SIDE_BOTTOM])
+            {
+                if (AdjacentPipeTiles[SIDE_TOP])
+                {
+                    if (AdjacentPipeTiles[SIDE_RIGHT])
+                    {
+                        return Enums.PipeTileSprite.OpenLeft;
+                    }
+                    else // right no
+                    {
+                        return Enums.PipeTileSprite.Vertical;
+                    }
+                }
+                else // top no
+                {
+                    if (AdjacentPipeTiles[SIDE_RIGHT])
+                    {
+                        return Enums.PipeTileSprite.CornerTopLeft;
+                    }
+                    else // right no
+                    {
+                        return Enums.PipeTileSprite.CapTop;
+                    }
+                }
+            }
+            else // bottom no
+            {
+                if (AdjacentPipeTiles[SIDE_TOP])
+                {
+                    if (AdjacentPipeTiles[SIDE_RIGHT])
+                    {
+                        return Enums.PipeTileSprite.CornerBottomLeft;
+                    }
+                    else // right no
+                    {
+                        return Enums.PipeTileSprite.CapBottom;
+                    }
+                }
+                else // top no
+                {
+                    if (AdjacentPipeTiles[SIDE_RIGHT])
+                    {
+                        return Enums.PipeTileSprite.CapLeft;
+                    }
+                }
+            }
+        }
 
-	#endif
+        return Enums.PipeTileSprite.Horizontal;
+    }
+
+
+    /// <summary>
+    /// Determines whether this instance is an adjacent tile (as opposed to a corner)
+    /// </summary>
+    /// <returns><c>true</c> if this instance is an adjacent tile; otherwise, <c>false</c>.</returns>
+    /// <param name="x">The x coordinate from center</param>
+    /// <param name="y">The y coordinate from center</param>
+    private bool IsAdjacentTile(int x, int y)
+    {
+        return ((x == -1 && y == 0) || (x == 0 && y == -1) || (x == 0 && y == 1) || (x == 1 && y == 0));
+    }
+
+    /// <summary>
+    /// Determines whether this instance has a pipe tile at the specified tilemap position.
+    /// </summary>
+    /// <returns><c>true</c> if this instance has a pipe tile the specified tilemap position; otherwise, <c>false</c>.</returns>
+    /// <param name="tilemap">The current tilemap</param>
+    /// <param name="position">the position to check</param>
+    private bool HasPipeTile(ITilemap tilemap, Vector3Int position)
+    {
+        return tilemap.GetTile(position) == this;
+    }
+
+#if UNITY_EDITOR
+
+    [MenuItem("Assets/Create/Tiles/PipeTile")]
+    public static void CreatePipeTile()
+    {
+        string path = EditorUtility.SaveFilePanelInProject("Save Pipe Tile", "New Pipe Tile", "asset", "Save Pipe Tile", "Assets");
+
+        if (path == "")
+            return;
+
+        AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<PipeTile>(), path);
+    }
+#endif
 }
