2d2517f24681d0ebfafedd73a0f1f43938a02127
andy <andrewmyers@gmail.com>
2017-12-26 23:27:00 -0800

    fixing some merge errors

--- src/Marimo/Marimo/Assets/Scripts/GrowController.cs
+++ src/Marimo/Marimo/Assets/Scripts/GrowController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class GrowController : MonoBehaviour
 {
-<<<<<<< HEAD
     /// <summary>
     /// A reference to the pipe tracer
     /// </summary>
@@ -67,56 +66,4 @@ public class GrowController : MonoBehaviour
 
         anim.SetFloat(Globals.ANIM_PARAM_NORM_TIME, m_currentGrowthLevel);
     }
-=======
-	/// <summary>
-	/// Muckle's current level of growth, where 0 is normal size and 1 is largest
-	/// </summary>
-	public float CurrentGrowthLevel;
-
-	/// <summary>
-	/// Inflate Muckle.
-	/// </summary>
-	/// <param name="anim">Muckle's primary Animator</param>
-    public void Grow(Animator anim)
-	{
-        if (CurrentGrowthLevel < 0.05f)
-		{
-			anim.Play(Globals.ANIMSTATE_MUCKLE_GROW);
-		}  
-
-        if (CurrentGrowthLevel > 0.95f) 
-		{
-            CurrentGrowthLevel = 1;
-		} else 
-		{
-            CurrentGrowthLevel += (Time.deltaTime * 3);
-		}
-
-        anim.SetFloat(Globals.ANIM_PARAM_NORM_TIME, CurrentGrowthLevel);
-        // Debug.Log(CurrentGrowthLevel);
-	}
-
-	/// <summary>
-	/// Deflate Muckle.
-	/// </summary>
-	/// <param name="anim">Muckle's primary Animator</param>
-	public void Shrink(Animator anim)
-	{
-        if (CurrentGrowthLevel <= 0)
-		{
-			anim.Play(Globals.ANIMSTATE_IDLE);
-			return;
-        } else if (CurrentGrowthLevel > 0 && CurrentGrowthLevel < 0.05f) 
-		{
-            CurrentGrowthLevel = 0;
-		} else 
-		{
-            anim.Play(Globals.ANIMSTATE_MUCKLE_GROW);
-            CurrentGrowthLevel -= (Time.deltaTime * 3);
-		}
-
-        anim.SetFloat(Globals.ANIM_PARAM_NORM_TIME, CurrentGrowthLevel);
-        //Debug.Log(CurrentGrowthLevel);
-	}
->>>>>>> added fully charged animations, trigger collider for charge ability
 }
